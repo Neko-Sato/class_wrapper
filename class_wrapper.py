3bdc47dc3b87aa812f87755d93195c05235ecfa1
Hisatoshi Shimizu <47973835+Neko-Sato@users.noreply.github.com>
2023-11-11 03:38:23 +0900

    Update class_wrapper.py

--- class_wrapper.py
+++ class_wrapper.py
@@ -1,35 +1,52 @@
-from typing import *
+from typing import TypeVar, Type, Any
 
 T = TypeVar('T')
 
 class _Wrapper:
+	__wrapper_base_type__: Type[T]
 	__wrapper_base__: T
-	def __init__(self: T, base: T) -> None:
-		self.__wrapper_base__ = base
+	@classmethod
+	def from_instance(cls: Type[T], instance: T) -> T:
+		self = super().__new__(cls)
+		self.__wrapper_base__ = instance
+		return self
+	def __init__(self: T, *args, **kwds) -> None:
+		self.__wrapper_base__ = self.__wrapper_base_type__(*args, **kwds)
 	def __getattribute__(self: T, __name: str) -> Any:
-		if __name != "__wrapper_base__":
-			try:
-				return self.__wrapper_base__.__getattribute__(__name)
-			except AttributeError:
-				pass
-		return super().__getattribute__(__name)
+		try:
+			return super().__getattribute__(__name)
+		except AttributeError:
+			pass
+		return self.__wrapper_base__.__getattribute__(__name)
 	def __setattr__(self: T, __name: str, __value: Any) -> None:
-		if __name != "__wrapper_base__":
-			try:
-				return self.__wrapper_base__.__setattr__(__name, __value)
-			except AttributeError:
-				pass
-		return super().__setattr__(__name, __value)
+		try:
+			return super().__setattr__(__name, __value)
+		except AttributeError:
+			pass
+		return self.__wrapper_base__.__setattr__(__name, __value)
+	def __class_getattribute__(cls: Type[T], __name: str) -> Any:
+		try:
+			return super().__class_getattribute__(__name)
+		except AttributeError:
+			pass
+		return cls.__wrapper_base_type__.__class_getattribute__(__name)
+	def __class_setattr__(cls: Type[T], __name: str, __value: Any) -> None:
+		try:
+			return super().__class_setattr__(__name, __value)
+		except AttributeError:
+			pass
+		return cls.__wrapper_base_type__.__class_setattr__(__name, __value)
 
 def ClassWrapper(_type: Type[T]) -> Type[T]:
-	##
 	class Wrapper(_Wrapper):
+		__wrapper_base_type__ = _type
 		__bases__ = _type, 
 		__name__ = _type.__name__
-		__qualname__ = f"Wrapper({_type.__qualname__})"
-		def __init__(self: T, *args, **kwds) -> None:
-			super().__init__(_type(*args, **kwds))
-	##
+		__qualname__ = f"{_Wrapper.__name__}[{_type.__qualname__}]"
 	return Wrapper
 
-__all__ = [ClassWrapper]
+def weak_instance(instance: T) -> T:
+	wrapper = ClassWrapper(instance.__class__)
+	return wrapper.from_instance(instance)
+
+__all__ = [ClassWrapper, weak_instance]
